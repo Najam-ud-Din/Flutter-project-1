8f2016980b0e3666979c3be48b0d715d3b9329a1
Najam-ud-Din <najamg377@gmail.com>
2023-09-21 20:21:33 +0500

    Add file to the github

---
+++ tutorial.dart
@@ -0,0 +1,86 @@
+void main(List<String> args) {
+  // print("Hello world");
+  //# valid variables names and rules
+  // ## valid variable
+  // var name = "Najam";
+  // var address23 = '''Block No 503 , ''';
+  // var value = 34542.54322;
+
+  // print(name);
+  // print(address23);
+  // print(value);
+  // print(address23.length);
+  // print(address23.runtimeType);
+
+  // int rty = 54;
+
+  // print(rty.runtimeType);
+  // print(rty.bitLength);
+
+  // print(rty.isEven);
+  // print(rty.isInfinite);
+
+  // double value = 54.34;
+  // print(value);
+  // print(value.runtimeType);
+  // print(value.isNegative);
+
+  // String value = "Najamherelove";
+  // print(value);
+  // print(value.length);
+  // print(value.runtimeType);
+  // print(value.contains("h"));
+  // print(value.endsWith('e'));
+  // print(value.replaceAll('el', "al"));
+  // print(value.hashCode);
+  // print(value.toLowerCase());
+
+  // var ytr = '''erjgofnfnmf
+  // rvkvnroro
+  // rlfpr
+  // rklrgprgmp4gj4pf4 nr
+  // v rg24po24pnl g24
+  // rv rgm24pk rb r24kfp4k brl''';
+
+  // print(ytr);
+  // print(ytr.length);
+  // print(ytr.runes);
+  // print(ytr.allMatches("nsjsm"));
+  // print(ytr.lastIndexOf('r'));
+
+  // bool value = true;
+  // print(value);
+  // print(value.runtimeType);
+  // print(value.hashCode);
+  // #Arithmetic operator
+  int a = 65, b = 32;
+
+  // print(a + b);
+  // print(a - b);
+  // print(a * b);
+  // print(a / b);
+  // print(a ~/ b);
+  // print(a % b);
+
+  //# type test operator
+//  print(a is int);
+
+  // String name = "najam";
+  // print(name is String);
+  // print(name is! int);
+
+  String email = "najamg377@gmail.com";
+  int password = 43421242;
+
+  bool condition;
+  condition = (email == "najamg377@gmail.com");
+
+  if (condition == true) {
+    print("Successfully login");
+  } else {
+    print("Wrong credentials");
+  }
+
+
+  
+}
